c7acf66b83a0b1355bac23aea4845293b8a63a96
Syed Saddam <youremail@example.com>
2025-10-06 16:13:49 +0500

    modify 2nd design screen

--- app/src/main/java/com/example/glowgreen/MainActivity.kt
+++ app/src/main/java/com/example/glowgreen/MainActivity.kt
@@ -18,30 +18,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GlowGreenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    GlowGreenTheme {
-        Greeting("Android")
-    }
+
 }
